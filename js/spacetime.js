20b5041b71336cb580d981a5a575247da1437d79
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2015-01-23 12:56:29 -0500

    :8ball: more stuff

--- js/spacetime.js
+++ js/spacetime.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
   $(".more").click( function (e) {
     e.preventDefault();
     d = $(this).data("more");
+    // TODO: add item to history to support back button?
     if (d) {
       id = "#" + d;
       $("body").scrollTo(id, 500, {easing:'easeOutCubic',offset: {top:-20}});
